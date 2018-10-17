80aa4cc0652d2e18bca6415265c81f3e5c445300
gadjetron <gi87365@yandex.ru>
2018-10-17 17:55:39 +0300

    Fixed layout of root 'PortfolioApp' component. Added component 'ContactForm' to the root 'PortfolioApp' component

--- src/PortfolioApp.vue
+++ src/PortfolioApp.vue
@@ -2,18 +2,21 @@
     <div id="portfolio-app">
         <Navbar></Navbar>
         <Portfolio></Portfolio>
+        <ContactForm></ContactForm>
     </div>
 </template>
 
 <script>
     import Navbar from "./components/navbar/Navbar.vue";
     import Portfolio from "./components/portfolio/Portfolio.vue";
+    import ContactForm from "./components/contact_form/ContactForm.vue";
 
     export default {
         name: 'PortfolioApp',
         components: {
             Navbar,
-            Portfolio
+            Portfolio,
+            ContactForm
         }
     };
 
@@ -22,11 +25,10 @@
 <style scoped>
     #portfolio-app{
         display: grid;
-        grid-template-rows: minmax(100px, auto) repeat(4, 1fr);
+        grid-template-rows: minmax(100px, auto) repeat(4, minmax(320px, auto));
         grid-template-areas: "navbar"
                              "portfolio"
                              "team"
-                             "clients_opinions"
                              "contact_form";
         grid-row-gap: 0.3em;
 
