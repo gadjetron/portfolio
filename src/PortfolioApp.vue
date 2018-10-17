536e4702ff79996686bf76d855e198ae6c504797
gadjetron <gi87365@yandex.ru>
2018-10-17 22:21:07 +0300

    Added component 'Team' to the root 'PortfolioApp' component

--- src/PortfolioApp.vue
+++ src/PortfolioApp.vue
@@ -2,31 +2,31 @@
     <div id="portfolio-app">
         <Navbar></Navbar>
         <Portfolio></Portfolio>
+        <Team></Team>
     </div>
 </template>
 
 <script>
     import Navbar from "./components/navbar/Navbar.vue";
     import Portfolio from "./components/portfolio/Portfolio.vue";
+    import Team from "./components/team/Team.vue";
 
     export default {
         name: 'PortfolioApp',
         components: {
             Navbar,
-            Portfolio
+            Portfolio,
+            Team
         }
     };
-
 </script>
 
 <style scoped>
     #portfolio-app{
         display: grid;
-        grid-template-rows: minmax(100px, auto) repeat(4, 1fr);
         grid-template-areas: "navbar"
                              "portfolio"
                              "team"
-                             "clients_opinions"
                              "contact_form";
         grid-row-gap: 0.3em;
 
